976cb39a1346f0c30002942a2920a716d46dae21
david siah <dsiah@villanova.edu>
2015-11-18 01:22:50 -0500

    added intro par

--- ellers.js
+++ ellers.js
@@ -18,9 +18,32 @@ Maze.prototype.set = function(x, y, val) {
 	return this.cells[index];
 }
 
-Maze.prototype.removeWall(x, y, direction) {
+Maze.prototype.removeWall = function(x, y, direction) {
 	// (x,y) coordinates of cell to mutate
-	// (direction) 
+	// (direction) 1, 2, 4, 8 corresponds to N, S, W, E
+	// (TODO) Implement Boundary check & implement other wall-removals
+	var cells = this.cells;
+
+	switch(direction) {
+		case 'N':
+			console.log('Changing the North wall');
+			return;
+		
+		case 'S':
+			console.log('Changing the South wall');
+			return;
+
+		case 'W':
+			console.log('Changing the West wall');
+			this.set(x, y, 4);
+			this.set(x - 1, y, 8);
+			return;
+
+		case 'E':
+			console.log('Changing the East wall');
+			return;
+	}
+
 }
 
 Maze.prototype.getRow = function (y) {
@@ -33,34 +56,40 @@ function chance() {
 
 function adjPave(row_sets, row_num, maze) {
 	if (row_sets.length <= 1) 
-		return row.sets;
+		return row_sets;
 
 	var cells = maze.cells,
 			prev,
 			curr;
-			
 
-	for (var ind = 1; ind < row.length; ind++) {
+	for (var ind = 1; ind < row_sets.length; ind++) {
 		prev = row_sets[ind - 1];
 		curr = row_sets[ind];
+		if (curr != prev && chance()) {
+			row_sets[ind] = prev;
+			maze.removeWall(ind, row_num, 'W')
+		}
 	} 
 
+	console.log(row_sets);
 }
 
 function eller(maze) {
 	var cells  = maze.cells,    // Cells of Maze
 			r_sets = maze.row_sets, // Temporary Cell-Set Mapping for row
-			c_set  = 0,							// Current Set 
+			c_set  = 1,							// Current Set 
 			c_row  = 0,             // Current Row Number
-			rows   = maze.y_dim;    // Number of rows in Maze
+			nrows   = maze.y_dim;    // Number of rows in Maze
 
 	// (Step 1) Initialize First Row
-	for (var m = 0; m < nrows - 2; m++) {
+	for (var m = 0; m < nrows; m++) {
 		r_sets[m] = c_set;
 		c_set++;
 	}
 
 	// (Step 2) Connect adjacent and disjoint cells at random
+	adjPave(r_sets, c_row, maze);
+	console.log(cells);
 
 	// (Step 3) Remove bottom walls randomly, each set must have at least one bottom wall removed
 
